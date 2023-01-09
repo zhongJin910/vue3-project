ac624c862b0bb35a4c6e08f20f552a41af3030b4
jin.zhong <jin.zhong@qizhidao.com>
2023-01-09 20:59:06 +0800

    feat:

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,35 +1,49 @@
 // 元素曝光
 class TargetExposure {
-  saveArr: any[];
-  shenceParams?: object;
-  fn: (() => any) | undefined;
+  private saveArr: any[];
+  private unObserve: IntersectionObserver[];
+  private shenceParams?: object;
+  private entries: any[];
+  callback: (() => any) | undefined;
+
   constructor(target: string, shenceParams?: object, fn?: () => any) {
     this.saveArr = [];
+    this.unObserve = [];
     this.shenceParams = shenceParams;
-    this.fn = fn;
-    this.init(target);
+    this.entries = [];
+    this.callback = fn;
+    this.install(target);
   }
-  init(target: string): void {
+  install(target: string): void {
     const self = this;
     try {
       const targetList: NodeListOf<Element> = document.querySelectorAll(target);
       targetList.forEach((item) => {
         const observer = new IntersectionObserver(this.intersect.bind(self));
         observer.observe(item);
+        this.unObserve.push(observer);
       });
     } catch (error) {
       console.log(error);
     }
   }
+  uninstall() {
+    this.unObserve.map((item) => {
+      item.unobserve(this.entries[0].target);
+    });
+  }
   intersect(entries: any): void {
+    this.entries = entries;
     try {
       if (!entries[0].isIntersecting) return;
       const id: any = entries[0].target.dataset.id;
+      console.log(entries[0].target, "entries[0].target.dataset");
+
       const opt: object = JSON.parse(entries[0].target.dataset.opt || "{}");
       const shenceOpt: object = this.shenceParams || opt;
       if (this.saveArr.includes(id)) return;
       console.log(id, shenceOpt, "shenceOpt");
-      if (typeof this.fn === "function") return this.fn();
+      if (typeof this.callback === "function") return this.callback();
       this.saveArr.push(id);
       // shenceReport({ ...shenceOpt });
     } catch (error) {
